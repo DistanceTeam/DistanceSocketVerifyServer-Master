f16e26c4dbd126d406f910e9e3255c5c7587e423
xiaozhanyi36 <xiaozhanyi36@gmail.com>
2023-07-01 23:56:03 +0800

    11

--- src/AuthServer.java
+++ src/AuthServer.java
@@ -3,49 +3,59 @@ import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-/**
- * Code by MiLiBlue, At 2022/11/12
- **/
+
 public class AuthServer {
 
-    public static AuthServer instance = new AuthServer();
-    public File key = new File("key.txt");
-    public File accounts = new File("Account.txt");
+    public static AuthServer instance = new AuthServer( );
+    public File key = new File( "key.txt" );
+    public File accounts = new File( "Account.txt" );
+    public File banlist = new File( "BanList.txt" );
+    public File irclog = new File( "IrcLog" );
     public String runPath = null;
-    public static void main(String[] args) throws IOException {
-        System.out.printf("Server start init...\n");
+
+    public static void main( String[] args ) throws IOException {
+        System.out.printf( "Server start init...\n" );
         try {
             // ���������socket
-            ServerSocket serverSocket = new ServerSocket(54130);
+            ServerSocket serverSocket = new ServerSocket( 8666 );
+
 
             // �����ͻ���socket
-            Socket socket = new Socket();
+            Socket socket = new Socket( );
 
             //��ȡ����Ŀ¼
-            AuthServer.instance.runPath = new File("").getCanonicalPath();
-            if(!AuthServer.instance.accounts.exists()){
-                instance.accounts.createNewFile();
+            AuthServer.instance.runPath = new File( "" ).getCanonicalPath( );
+            if ( !AuthServer.instance.accounts.exists( ) ) {
+                instance.accounts.createNewFile( );
             }
-            if(!AuthServer.instance.key.exists()){
-                instance.key.createNewFile();
+            if ( !AuthServer.instance.key.exists( ) ) {
+                instance.key.createNewFile( );
+            }
+            if ( !AuthServer.instance.banlist.exists( ) ) {
+                instance.key.createNewFile( );
+            }
+            if ( !AuthServer.instance.irclog.exists( ) ) {
+                instance.key.createNewFile( );
             }
 
-            System.out.printf("Server init done!");
+
+            System.out.printf( "Server init done!" );
 
             //ѭ�������ȴ��ͻ��˵�����
-            while(true){
+            while ( true ) {
                 // �����ͻ���
-                socket = serverSocket.accept();
+                socket = serverSocket.accept( );
 
-                AuthServerThread thread = new AuthServerThread(socket);
-                thread.start();
 
-                InetAddress address=socket.getInetAddress();
-                System.out.println("New Client Connect! IP:"+address.getHostAddress());
+                AuthServerThread thread = new AuthServerThread( socket );
+                thread.start( );
+
+                InetAddress address = socket.getInetAddress( );
+                System.out.println( "New Client Connect! IP:" + address.getHostAddress( ) );
             }
-        } catch (Exception e) {
+        } catch ( Exception e ) {
             // TODO: handle exception
-            e.printStackTrace();
+            e.printStackTrace( );
         }
     }
 }
